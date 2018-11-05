97bf9de1de9b182b8410a4d8f364aacc9f38de10
KetchupOnWaffles <44327873+KetchupOnWaffles@users.noreply.github.com>
2018-11-05 11:07:17 +0100

    Update effet de serre.py

--- effet de serre.py
+++ effet de serre.py
@@ -3,17 +3,17 @@ from scipy.optimize import *
 
 
 #inputs:
-T = float(input("T = "))
-Fd = float(input("Fd = "))
-Fi = float(input("Fi = "))
+T = float(input("T (K)= "))
+Fd = float(input("Fd (W/m^2)= "))
+Fi = float(input("Fi (W/m^2)= "))
 g = 5.67 * (10**(-8))
 h = 4
 def syst(z):
     P = z[0]
     Ts = z[1]
     Tp = z[2]
-    Fp = z[3]
-    Fs = z[4]
+    Fs = z[3]
+    Fp = z[4]
     F = empty((5))
     F[0] = h*(Ts-T) + h*(Tp-T) - P
     F[1] = Fp + P - Fd - Fi
@@ -21,6 +21,6 @@ def syst(z):
     F[3] = g*(Tp**4) - Fp
     F[4] = g*(Ts**4) - Fs
     return F
-xGuess = array([1,1,1,1,1])
-z = fsolve(syst,xGuess)
+zGuess = array([1,1,1,1,1])
+z = fsolve(syst,zGuess)
 print(z)
